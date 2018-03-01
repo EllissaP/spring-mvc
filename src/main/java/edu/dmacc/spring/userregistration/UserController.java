09b5d2f7eb8bbe21bed52716454aa702bc83f290
EllissaP <ellissa.peterson@gmail.com>
2018-02-28 21:40:28 -0600

    first commit

---
+++ src/main/java/edu/dmacc/spring/userregistration/UserController.java
@@ -0,0 +1,38 @@
+package edu.dmacc.spring.userregistration;
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Bean;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.servlet.ModelAndView;
+
+@Controller
+public class UserController 
+{
+	@Autowired UserDao dao;
+	private static final String[ ] countries = { "France", "United States", "Germany", "England" };
+	@RequestMapping(value = "/result")
+	public ModelAndView processUser(User user){
+		ModelAndView modelAndView = new ModelAndView();
+		dao.insertUser(user);
+		modelAndView.setViewName("userResult");
+		modelAndView.addObject("u", user);
+		return modelAndView;
+	}
+	public ModelAndView viewAll( ){
+		ModelAndView modelAndView = new ModelAndView();
+		List<User> allUsers = dao.getAllUsers();
+		modelAndView.setViewName("viewAllUsers");
+		modelAndView.addObject("all", allUsers);
+		return modelAndView;
+	}
+
+
+	@Bean
+	public UserDao dao(){
+		UserDao bean = new UserDao();
+		return bean;
+	}
+
+}
